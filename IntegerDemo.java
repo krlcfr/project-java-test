3ea4f24aa43860e78e2b17450dcb438ee4cf8f7c
Santiago <santiago.gallego28@outlook.com>
2025-11-07 16:21:34 -0500

    o feat: operaciones aritméticas básicas

---
+++ IntegerDemo.java
@@ -0,0 +1,23 @@
+public class IntegerDemo {
+    public static void main(String[] args) {
+         int number = 42;
+            System.out.println("El numero es: " + number);
+            //seccion de aritmetica basica
+             int number1 = 100;
+             int number2 = 2;
+
+             int sum = number1 + number2;
+            int difference = number1 - number2;
+            int product = number1 * number2;
+            int quotient = number1 / number2;
+
+            System.out.println("Number 1: " + number1);
+            System.out.println("Number 2: " + number2);
+            System.out.println("Sum: " + sum);
+            System.out.println("Difference: " + difference);
+            System.out.println("Product: " + product);
+            System.out.println("Quotient: " + quotient);
+
+
+    }
+}
